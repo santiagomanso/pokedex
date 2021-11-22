21fe11b2db106e507922cb610b8337e4254218ed
Santiago <santiagomanso@hotmail.com>
2021-11-22 01:34:53 +0100

    new get petition with axios, act indic. etc

---
+++ src/components/PokemonDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const PokemonDetails = () => {
+    return (
+        <View>
+            <Text>Hola Mundo</Text>
+        </View>
+    )
+}
--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,21 +1,121 @@
 import React from 'react'
 import { StackScreenProps } from '@react-navigation/stack'
-import { Text } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, Image, ActivityIndicator } from 'react-native';
 import { RootStackParams } from '../navigator/Navigator'
-import { SimplePokemon } from '../interfaces/pokemonInterfaces';
+
+
+import Icon from 'react-native-vector-icons/Ionicons'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FadeInImage } from '../components/FadeInImage';
+import { usePokemon } from '../hooks/usePokemon';
 
 interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> {};
 
 
 export const PokemonScreen = ( { navigation, route }:Props ) => {
 
-    const { simplePokemon, color } = route.params
+    const { simplePokemon, color } = route.params;
+    const { id, name, picture } = simplePokemon;
+    const { top } = useSafeAreaInsets();
 
+    const {isLoading, pokemon} =  usePokemon( id )
     
 
     return (
-        <>
-            <Text>{ simplePokemon.name } - { color} </Text>
-        </>
+       <View style={{flex: 1}}>
+            {/*Header Container*/}
+            <View style={{
+                    ...styles.headerContainer,
+                    backgroundColor: color
+            }}>
+            
+                <TouchableOpacity
+                    onPress={ ()=> navigation.pop() }
+                    activeOpacity={0.92}
+                    style={{
+                        ...styles.backButton,
+                        top: top + 10
+                    }}
+                >
+                    <Icon name="arrow-back-outline" color="white" size={30}></Icon>
+                </TouchableOpacity>
+
+                {/* Pokemon Name */}    
+                <Text style={{
+                    ...styles.pokemonName,
+                    top: top + 45
+                }}>
+                    { name + '\n' } #{id}
+                </Text>
+
+                {/* White Pokebol */}
+                <Image 
+                    source={require('../assets/pokebola-blanca.png')}
+                    style={{
+                        ...styles.pokeball
+                    }}
+                />
+
+                {/* Pokemon Image */}
+                <FadeInImage 
+                    uri={ picture }
+                    style={{
+                        ...styles.pokemonImage
+                    }}
+                />
+
+            </View>  
+
+            {/* Details */}
+            <View style={{
+                ...styles.loadingIndicator
+            }}>
+                <ActivityIndicator 
+                    color={color}
+                    size={50}
+                />
+            </View>
+
+        </View>
+      
     )
 }
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        height: 370,
+        zIndex: 999,
+        alignItems: "center",
+        borderBottomLeftRadius: 1000,
+        borderBottomRightRadius: 1000
+    },
+    backButton:{
+        position: "absolute",
+        size: 35,
+        left: 20,
+    },
+    pokemonName: {
+        color: 'white',
+        fontSize: 40,
+        alignSelf: 'flex-start',
+        left: 20
+    },
+    pokeball:{
+        width: 250,
+        height: 250,
+        bottom: -10,
+        opacity: 0.7
+    },
+    pokemonImage:{
+        width: 250,
+        height: 250,
+        position: 'absolute',
+        bottom: -20
+    },
+    loadingIndicator:{
+        flex: 1,
+        
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
